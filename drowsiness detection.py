f7e94d78f586ba76ad8ab5965550c6015d62e62a
lokita282 <lokitavarma282@gmail.com>
2022-05-01 10:05:48 +0530

    multiple people detection done

--- drowsiness detection.py
+++ drowsiness detection.py
@@ -25,7 +25,6 @@ i=0
 count=0
 score=0
 state=0
-total=0
 thicc=2
 rpred=[99]
 lpred=[99]
@@ -43,58 +42,96 @@ while(True):
     for(ex, ey, ew, eh) in faces:
         cv2.rectangle(frame, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
         cv2.putText(frame, 'Face #' + str(face_count), (ex, ey), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,0), 2)
-        print(faces)
-        print('------------')
-        print(faces.shape)
+        # print(faces)
+        # print('------------')
+        # print(faces.shape)
         face_count += 1
+        for (x,y,w,h) in right_eye:
+            r_eye=frame[y:y+h,x:x+w]
+            count=count+1
+            r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
+            r_eye = cv2.resize(r_eye,(24,24))
+            r_eye= r_eye/255
+            r_eye=  r_eye.reshape(24,24,-1)
+            r_eye = np.expand_dims(r_eye,axis=0)
+            # rpred = model.predict_classes(r_eye)
+            rpred = np.argmax(model.predict(r_eye), axis=-1)            #updated this line. upar wali line is outdated
+            if(rpred[0]==1):
+                lbl='Open' 
+            if(rpred[0]==0):
+                lbl='Closed'
+            break
+        for (x,y,w,h) in left_eye:
+            l_eye=frame[y:y+h,x:x+w]
+            count=count+1
+            l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
+            l_eye = cv2.resize(l_eye,(24,24))
+            l_eye= l_eye/255
+            l_eye=l_eye.reshape(24,24,-1)
+            l_eye = np.expand_dims(l_eye,axis=0)
+            #lpred = model.predict_classes(l_eye)
+            lpred = np.argmax(model.predict(l_eye), axis=-1)            #updated this line. upar wali line is outdated
+            if(lpred[0]==1):
+                lbl='Open'   
+            if(lpred[0]==0):
+                lbl='Closed'
+            break
+        if(rpred[0]==0 and lpred[0]==0):
+            score=score+1
+        # if(rpred[0]==1 or lpred[0]==1):
+        else:
+            score=score-1
+        if(score<0):    
+            score=0
+        cv2.putText(frame,'Score:'+str(score),(ex,ey-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+        
 
-    cv2.rectangle(frame, (0,height-50) , (200,height) , (0,0,0) , thickness=cv2.FILLED )
+    # cv2.rectangle(frame, (0,height-50) , (200,height) , (0,0,0) , thickness=cv2.FILLED )
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y) , (x+w,y+h) , (100,100,100) , 1 )
-        # total += 1
+    # for (x,y,w,h) in faces:
+    #     cv2.rectangle(frame, (x,y) , (x+w,y+h) , (100,100,100) , 1 )
 
-    for (x,y,w,h) in right_eye:
-        r_eye=frame[y:y+h,x:x+w]
-        count=count+1
-        r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
-        r_eye = cv2.resize(r_eye,(24,24))
-        r_eye= r_eye/255
-        r_eye=  r_eye.reshape(24,24,-1)
-        r_eye = np.expand_dims(r_eye,axis=0)
-        # rpred = model.predict_classes(r_eye)
-        rpred = np.argmax(model.predict(r_eye), axis=-1)            #updated this line. upar wali line is outdated
-        if(rpred[0]==1):
-            lbl='Open' 
-        if(rpred[0]==0):
-            lbl='Closed'
-        break
+    # for (x,y,w,h) in right_eye:
+    #     r_eye=frame[y:y+h,x:x+w]
+    #     count=count+1
+    #     r_eye = cv2.cvtColor(r_eye,cv2.COLOR_BGR2GRAY)
+    #     r_eye = cv2.resize(r_eye,(24,24))
+    #     r_eye= r_eye/255
+    #     r_eye=  r_eye.reshape(24,24,-1)
+    #     r_eye = np.expand_dims(r_eye,axis=0)
+    #     # rpred = model.predict_classes(r_eye)
+    #     rpred = np.argmax(model.predict(r_eye), axis=-1)            #updated this line. upar wali line is outdated
+    #     if(rpred[0]==1):
+    #         lbl='Open' 
+    #     if(rpred[0]==0):
+    #         lbl='Closed'
+    #     break
 
-    for (x,y,w,h) in left_eye:
-        l_eye=frame[y:y+h,x:x+w]
-        count=count+1
-        l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
-        l_eye = cv2.resize(l_eye,(24,24))
-        l_eye= l_eye/255
-        l_eye=l_eye.reshape(24,24,-1)
-        l_eye = np.expand_dims(l_eye,axis=0)
-        #lpred = model.predict_classes(l_eye)
-        lpred = np.argmax(model.predict(l_eye), axis=-1)            #updated this line. upar wali line is outdated
-        if(lpred[0]==1):
-            lbl='Open'   
-        if(lpred[0]==0):
-            lbl='Closed'
-        break
+    # for (x,y,w,h) in left_eye:
+    #     l_eye=frame[y:y+h,x:x+w]
+    #     count=count+1
+    #     l_eye = cv2.cvtColor(l_eye,cv2.COLOR_BGR2GRAY)  
+    #     l_eye = cv2.resize(l_eye,(24,24))
+    #     l_eye= l_eye/255
+    #     l_eye=l_eye.reshape(24,24,-1)
+    #     l_eye = np.expand_dims(l_eye,axis=0)
+    #     #lpred = model.predict_classes(l_eye)
+    #     lpred = np.argmax(model.predict(l_eye), axis=-1)            #updated this line. upar wali line is outdated
+    #     if(lpred[0]==1):
+    #         lbl='Open'   
+    #     if(lpred[0]==0):
+    #         lbl='Closed'
+    #     break
 
-    if(rpred[0]==0 and lpred[0]==0):
-        score=score+1
-    # if(rpred[0]==1 or lpred[0]==1):
-    else:
-        score=score-1
-    i=0   
-    if(score<0):
-        score=0   
-    cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
+    # if(rpred[0]==0 and lpred[0]==0):
+    #     score=score+1
+    # # if(rpred[0]==1 or lpred[0]==1):
+    # else:
+    #     score=score-1
+    # i=0   
+    # if(score<0):
+    #     score=0   
+    # cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
     if(score>15):
         #person is feeling sleepy so we beep the alarm
         cv2.imwrite(os.path.join(path,'image.jpg'),frame)
@@ -118,7 +155,6 @@ while(True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 print(state)
-print(total)
 print(face_count)
 cap.release()
 cv2.destroyAllWindows()
